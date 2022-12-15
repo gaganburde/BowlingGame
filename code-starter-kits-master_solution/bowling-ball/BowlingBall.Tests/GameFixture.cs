9b2b947a9407bb06db9443ce6e4e3ce77053421a
gaganburde <gagan.burde@gmail.com>
2022-12-15 12:48:54 +0530

    added solution for bowling

---
+++ code-starter-kits-master_solution/bowling-ball/BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game gameInstance;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            gameInstance = new Game();
+        }
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            gameInstance = new Game();
+            Roll(gameInstance, 0, 20);
+            Assert.AreEqual(0, gameInstance.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+        [TestMethod]
+        public void TestSpare()
+        {
+
+            gameInstance.Roll(5);
+            gameInstance.Roll(5);
+            gameInstance.Roll(4);
+            Roll(gameInstance, 0, 17);
+            Assert.AreEqual(18, gameInstance.GetScore());
+        }
+
+        [TestMethod]
+        public void TestAllOne()
+        {
+            Roll(gameInstance, 1, 20);
+            Assert.AreEqual(20, gameInstance.GetScore());
+        }
+
+        [TestMethod]
+        public void TestStrike()
+        {
+            gameInstance.Roll(10);
+            gameInstance.Roll(5);
+            gameInstance.Roll(4);
+            Roll(gameInstance, 0, 17);
+            Assert.AreEqual(28, gameInstance.GetScore());
+        }
+
+        [TestMethod]
+        public void TestAllStrike()
+        {
+            
+            Roll(gameInstance, 10, 21);
+            Assert.AreEqual(300, gameInstance.GetScore());
+        }
+
+        [TestMethod]
+        public void TestAllSpare()
+        {
+
+            Roll(gameInstance, 5, 21);
+            Assert.AreEqual(150, gameInstance.GetScore());
+        }
+
+    }
+}
---
+++ code-starter-kits-master_solution/bowling-ball/BowlingBall/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        const int MAXNOOFROLLS = 21;
+        private int[] rolls = new int[MAXNOOFROLLS];
+
+        private int currentRole = 0;
+        public void Roll(int pins)
+        {
+            rolls[currentRole++] = pins;
+            // Add your logic here. Add classes as needed.
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int rollCounter = 0;
+            const int MaxFrameCount = 10;
+            for (int frame = 0; frame < MaxFrameCount; frame++)
+            {
+                if (IsSpare(rollCounter))
+                {
+                    score += 10 + GetSpareBonus(rollCounter);
+                    rollCounter += 2;
+                }
+                else if (IsStrike(rollCounter))
+                {
+                    score += 10 + GetStrikeBonus(rollCounter);
+                    rollCounter++;
+                }
+                else
+                {
+                    score += SunOfBallsInFrame(rollCounter);
+                    rollCounter += 2;
+
+                }
+
+            }
+            // Returns the final score of the game.
+            return score;
+        }
+
+        private int GetSpareBonus(int rollCounter)
+        {
+            return rolls[rollCounter + 2];
+        }
+
+        private int GetStrikeBonus(int rollCounter)
+        {
+            return rolls[rollCounter + 1] + rolls[rollCounter + 2];
+        }
+
+        private int SunOfBallsInFrame(int rollCounter)
+        {
+            return rolls[rollCounter] + rolls[rollCounter + 1];
+        }
+
+        private bool IsStrike(int rollCounter)
+        {
+            return rolls[rollCounter] == 10;
+        }
+
+        private bool IsSpare(int rollCounter)
+        {
+            return rolls[rollCounter] + rolls[rollCounter + 1] == 10;
+        }
+    }
+}
+
